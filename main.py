0327fd2107720fc05f2694eacfd5e3610c5b356a
Mohamed Benzarti <mohamed.benzarti@rebike.com>
2023-11-03 11:19:01 +0100

    test

--- main.py
+++ main.py
@@ -238,4 +238,3 @@ async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
-({', '.join(['%s' for _ in values])})
